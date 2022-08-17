d6f2f20ac8c46e93031815331c74ae6f69b0285b
Jacson Passold <jacson@passold.com.br>
2022-08-17 10:25:42 -0300

    include post_type project to default divi search

--- divi-post-type-blog.php
+++ divi-post-type-blog.php
@@ -27,3 +27,42 @@ if(!defined('__DIR__')) {
 }
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'wp-divi'. DIRECTORY_SEPARATOR .'modules.php';
+
+//Add projects to search
+function rl_custom_search( $query = false ) {
+	if ( is_admin() || ! is_a( $query, 'WP_Query' ) || ! $query->is_search ) {
+		return;
+	}
+	
+	$utils = ET_Core_Data_Utils::instance();
+	
+	// phpcs:disable WordPress.Security.NonceVerification.NoNonceVerification
+	if ( isset( $_GET['et_pb_searchform_submit'] ) ) {
+		$postTypes = array();
+		if ( ! isset($_GET['et_pb_include_posts'] ) && ! isset( $_GET['et_pb_include_pages'] ) ) $postTypes = array( 'post' );
+		if ( isset( $_GET['et_pb_include_pages'] ) ) $postTypes[] = 'page';
+		if ( isset( $_GET['et_pb_include_posts'] ) ) $postTypes[] = 'post';
+		
+		$postTypes[] = 'project';
+		// $postTypes is whitelisted values only
+		$query->set( 'post_type', $postTypes );
+		
+		if ( ! empty( $_GET['et_pb_search_cat'] ) ) {
+			$categories_array = explode( ',', $_GET['et_pb_search_cat'] );
+			$categories_array = $utils->sanitize_text_fields( $categories_array );
+			$query->set( 'category__not_in', $categories_array );
+		}
+		
+		if ( isset( $_GET['et-posts-count'] ) ) {
+			$query->set( 'posts_per_page', (int) $_GET['et-posts-count'] );
+		}
+	}
+	// phpcs:enable
+}
+
+function rl_remove_default_et_pb_custom_search() {
+	remove_action( 'pre_get_posts', 'et_pb_custom_search' );
+	add_action( 'pre_get_posts', 'rl_custom_search' );
+}
+add_action( 'wp_loaded', 'rl_remove_default_et_pb_custom_search' );
+
